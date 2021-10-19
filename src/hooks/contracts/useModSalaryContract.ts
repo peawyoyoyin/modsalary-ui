2a48d2392200636314280c63552cb76150d0e5a5
Piriyapong Laopongsit <peawyoyoyin@gmail.com>
2021-10-20 01:57:18 +0800

    fix claim not working :P

--- src/hooks/contracts/useModSalaryContract.ts
+++ src/hooks/contracts/useModSalaryContract.ts
@@ -8,5 +8,5 @@ import { ChainId, ChainIdToProviderId } from "../../constants/chain";
 export const useModSalaryContract = (chainId: ChainId, useInjected: boolean = false) => {
   const providerId = useInjected ? ProviderIds.Injected : ChainIdToProviderId[chainId];
 
-  return useContract(addresses.modSalary[chainId], modSalaryAbi, { providerId });
+  return useContract(addresses.modSalary[chainId], modSalaryAbi, { providerId, withSignerIfPossible: useInjected });
 }
